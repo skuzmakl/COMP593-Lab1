e0b0795cc11625adef93f0cee7150d7a3e111914
skuzmakl <122819867+skuzmakl@users.noreply.github.com>
2023-01-23 13:06:32 -0500

    Added comment to script1.py

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
+# Print lines from Zen of Python
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
