7e9f89d4cc127c63f57a0cf65beb4c38233ba461
skuzmakl <122819867+skuzmakl@users.noreply.github.com>
2023-01-23 12:56:42 -0500

    Created script files
    Added code that prints lines from Zen of Python.

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("There should be one-- and preferably only one --obvious way to do it.")
+print("Although that way may not be obvious at first unless you're Dutch.")
---
+++ script2.py
@@ -0,0 +1,3 @@
+print("Beautiful is better than ugly.")
+print("Explicit is better than implicit.")
+print("Simple is better than complex.")
